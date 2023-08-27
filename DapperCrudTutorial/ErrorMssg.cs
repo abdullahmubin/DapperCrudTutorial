97a7670961a18a37e5e717bf29677f9e5cbcdfb2
mubinCodemen <codemen.almubin@gmail.com>
2023-08-27 19:34:27 +0600

    Add project files.

---
+++ DapperCrudTutorial/ErrorMssg.cs
@@ -0,0 +1,8 @@
+﻿namespace DapperCrudTutorial
+{
+    public class ErrorMssg
+    {
+        public string ErrorMessage { get; set; } = string.Empty;
+        public string ErrorDetail { get; set; } = string.Empty;
+    }
+}
